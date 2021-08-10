e7021e7151f5e821c8af06bcfd5d0dc563ec1fd3
lissandro <lissandroperf7@gmail.com>
2021-08-10 08:10:47 -0300

    Resolvendo Problemas de Calculos ex -Kata-2-JS

--- katas2.js
+++ katas2.js
@@ -1,31 +1,76 @@
-// comece a criar a sua função add na linha abaixo
+function add (x,y){
+
+    console.log(x,y)
+
+    let soma = x + y ;
+
+    return soma;
+
+}
+
 
 
 // descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+
+//console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
-// comece a criar a sua função multiply na linha abaixo
+function multiply (point1,point2){
+
+   let mult =  point1;
+
+    for(let c = 1; c < point2 ; c++)
+    {
+    mult = add(mult , point1);
+
+    
+}
+return mult;
+}
 
 
 // descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+//console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+
+function power(point1,point2){
+
+    let potencia = point1;
+
+    for(let c = 1 ; c < point2 ; c++)
+    {
+
+        potencia = multiply(potencia,point1);
 
 
-// comece a criar a sua função power na linha abaixo
+    } 
+    return potencia;
+}
 
+// console.log(power(3,4));
 
 // descomente a linha seguinte para testar sua função
 // console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
-// comece a criar a sua função factorial na linha abaixo
+function factorial (ponto1){
+    
+    let aglomerar = ponto1;
+
+    for(let c = ponto1-1 ; c >= 1 ; c --)
+    {
+    aglomerar = multiply(aglomerar , c );
+
+    }
+    return aglomerar;
+}
+
+console.log(factorial(5))
 
 
 // descomente a linha seguinte para testar sua função
 // console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
-
+//// PRETENDO FAZER O BONUS NO FINAL DE SEMANA SE FOR POSSIVEL.
 /**
  * BONUS (aviso: o grau de dificuldade é bem maior !!!)
  */
